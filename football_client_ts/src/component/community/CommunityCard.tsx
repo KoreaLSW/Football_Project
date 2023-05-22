79842f632b84d5409e5977dd5ac39976c8880714
sangwoo <sagnwoo0560@gmail.com>
2023-05-22 14:54:56 +0900

    football project commit

---
+++ football_client_ts/src/component/community/CommunityCard.tsx
@@ -0,0 +1,38 @@
+import { communityRead } from '../../type/type';
+import styles from './CommunityCard.module.css';
+import { useNavigate } from 'react-router-dom';
+
+interface Props {
+    data: communityRead;
+}
+
+export function CommunityCard({ data }: Props) {
+    const movePage = useNavigate();
+
+    const handleClick = () => {
+        movePage('/Community/CommunityRead', { state: { data } });
+    };
+
+    return (
+        <li className={styles.item} onClick={handleClick}>
+            <div className={styles.title}>
+                <span>{data.community_title}</span>
+            </div>
+            <div className={styles.user}>
+                <a href={data.user_img} target='_blank'>
+                    <img src={data.user_img} alt='' />
+                </a>
+                <span>{data.user_nickname}</span>
+            </div>
+            <div className={styles.date}>
+                <span>{data.community_date}</span>
+            </div>
+            <div className={styles.views}>
+                <span>{data.community_view}</span>
+            </div>
+            <div className={styles.thumb}>
+                <span>{data.community_commend}</span>
+            </div>
+        </li>
+    );
+}
